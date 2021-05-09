b997b643e5adec507f8db2bcd417158e8a7bc384
Batuhan Sahin <battusahin97@gmail.com>
2021-05-09 06:58:30 +0300

    3.hafta 2.ders

---
+++ javaCamp/northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -0,0 +1,13 @@
+package kodlamaio.northwind.business.abstracts;
+
+import java.util.List;
+
+import kodlamaio.northwind.entities.concretes.Product;
+
+public interface ProductService {
+
+	List<Product> getAll();
+	
+	
+	
+}
