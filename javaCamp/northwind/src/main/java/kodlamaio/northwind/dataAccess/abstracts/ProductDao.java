e39955fe52c665935db73e35af266b9b6fc4ba45
Batuhan Sahin <battusahin97@gmail.com>
2021-05-25 21:27:08 +0300

    7. Ders

--- javaCamp/northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
+++ javaCamp/northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import kodlamaio.northwind.entities.concretes.Product;
 
-public interface ProductDao extends JpaRepository<Product,Integer> {
-	//JpaRepository bir tablo(nesne) ve o tablodaki primarykey typesini alir.Bunun icinde CRUD islemleri vardir.
+public interface ProductDao extends JpaRepository<Product,Integer> { //JpaRepository bir tablo(nesne) ve o tablodaki primarykey typesini alir.Bunun icinde CRUD islemleri vardir.
+	
+	
+	
 }
