f51a85c35296a7bf0c3331076bfb140096de9dcd
Batuhan Sahin <battusahin97@gmail.com>
2021-05-30 20:53:59 +0300

    8. Ders

--- javaCamp/northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
+++ javaCamp/northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -12,18 +12,24 @@ public interface ProductDao extends JpaRepository<Product, Integer> { // JpaRepo
 																		// islemleri vardir.
 
 	Product getByProductName(String productName);
-
+	//select * from products where product_name= abc
+	
 	Product getByProductNameAndCategory_CategoryId(String productName, int categoryId);
-
+	//select * from products where product_name= abc and category_id= 1
+	
 	List<Product> getByProductNameOrCategory_CategoryId(String productName, int categoryId);
-
+	//select * from products where product_name= abc or category_id= 1
+	
 	List<Product> getByCategoryIn(List<Integer> categories);
-
+	//select * from products where category_id in(1,2,3,4)
+	
 	List<Product> getByProductNameContains(String productName);
-
+	//select * from products where product_name LIKE '%product_name%'
+	
 	List<Product> getByProductNameStartsWith(String productName);
-
+	//select * from products where product_name LIKE 'product_name%'
+	
 	@Query("From Product where productName=:productName and category.categoryId=:categoryId")
 	List<Product> getByNameAndCategory(String productName, int categoryId);
-
+	//select * from products where product_name=bisey and category_id=bisey
 }
